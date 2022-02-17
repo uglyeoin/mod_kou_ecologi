b2cc9d6a6ac6356f39ac455210b578d991c12ba3
bharath kumar <[bharath@tealorca.in]>
2022-02-17 12:01:38 +0530

    Clean Up

--- mod_kou_ecologi.php
+++ mod_kou_ecologi.php
@@ -2,10 +2,10 @@
 /**
  * @package    mod_kou_ecologi
  *
- * @author     Eoin <your@email.com>
- * @copyright  A copyright
+ * @author     Eoin Oliver <eion@squareballoon.co.uk>
+ * @copyright  Onion Marketing Ltd
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
- * @link       http://your.url.com
+ * @link       https://squareballoon.co.uk
  */
 
 defined('_JEXEC') or die;
